d8ca9b290674ab0af0eb87f89b27e3ba9f59c2d9
Shiva-devloper <shivamalviya570@gmail.com>
2024-11-07 18:23:24 +0530

    Add files via upload

---
+++ shiva.java
@@ -0,0 +1,18 @@
+ 1import java.util.*;
+ public class shiva{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a first number");
+        int a= sc.nextInt();
+        System.out.println("enter a second number ");
+        int b= sc.nextInt();
+        System.out.println("enter a third number ");
+        int c=sc.nextInt();
+       int avg =(a+b+c)/3;
+       System.out.println("the average of three number is ");
+        System.out.print(avg);
+
+    }
+
+}
+
